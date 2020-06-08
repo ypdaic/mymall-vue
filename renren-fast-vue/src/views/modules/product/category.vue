d3e73a6a99b60001d3536c2c85d8a93bb6733fa6
ypdai <841943896@qq.com>
2020-06-09 00:59:59 +0800

    代码提交。

---
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,130 @@
+<!--  -->
+<template>
+    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node="false"
+        show-checkbox
+        node-key="catId">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            // 1,2级菜单才显示添加按钮
+            v-if="node.level <=2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            // 没有子节点才显示删除按钮
+            v-if="node.childNodes.length==0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+//import引入的组件需要注入到对象中才能使用
+components: {},
+// :data="menus" 会引用到
+data() {
+      return {
+        menus: [],
+        defaultProps: {
+          // 取menus数据展示
+          children: 'children',
+          label: 'name'
+        }
+      };
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      },
+      // 查询分类数据
+      getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get"
+      }).then(({ data }) => {
+        console.log("成功获取到菜单数据...", data.data);
+        this.menus = data.data;
+      });
+    },
+    append(data) {
+        console.log("append", data);
+        this.dialogType = "add";
+        this.title = "添加分类";
+        this.dialogVisible = true;
+        this.category.parentCid = data.catId;
+        this.category.catLevel = data.catLevel * 1 + 1;
+        this.category.catId = null;
+        this.category.name = "";
+        this.category.icon = "";
+        this.category.productUnit = "";
+        this.category.sort = 0;
+        this.category.showStatus = 1;
+      },
+
+      remove(node, data) {
+        var ids = [data.catId];
+        this.$confirm(`是否删除【${data.name}】菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+      .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false)
+          }).then(({ data }) => {
+            this.$message({
+              message: "菜单删除成功",
+              type: "success"
+            });
+            //刷新出新的菜单
+            this.getMenus();
+            //设置需要默认展开的菜单
+            this.expandedKey = [node.parent.data.catId];
+          });
+        })
+        .catch(() => {});
+
+        console.log("remove", node, data);
+      }
+    },
+//监听属性 类似于data概念
+computed: {},
+//监控data中的数据变化
+watch: {},
+
+//生命周期 - 创建完成（可以访问当前this实例）
+created() {
+    this.getMenus();
+},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {
+
+},
+beforeCreate() {}, //生命周期 - 创建之前
+beforeMount() {}, //生命周期 - 挂载之前
+beforeUpdate() {}, //生命周期 - 更新之前
+updated() {}, //生命周期 - 更新之后
+beforeDestroy() {}, //生命周期 - 销毁之前
+destroyed() {}, //生命周期 - 销毁完成
+activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style lang='scss' scoped>
+//@import url(); 引入公共css类
+
+</style>
